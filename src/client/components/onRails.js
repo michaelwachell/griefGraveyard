76d58bc10a0c2dc7e74c563d00e570125818fe01
Michael Wachell <wachell@gmail.com>
2019-01-23 16:24:25 -0800

    doinks

--- src/client/components/onRails.js
+++ src/client/components/onRails.js
@@ -29,30 +29,29 @@ AFRAME.registerComponent('collider-check', {
   dependencies: ['raycaster'],
 
   init() {
-    const animation = document.createElement('a-animation');
 
-    const me = this.el;
-    const x = me.object3D.position.x;
-    const y = me.object3D.position.y;
-    const z = me.object3D.position.z;
-    const newX = x + 2;
-    const newY = y + 2;
-    const newZ = z + 2;
-
-    setAttributes(animation, {
-      duration: '4000',
-      attribute: 'position',
-      from: `${x} ${y} ${z}`,
-      to: `${newX} ${newY} ${newZ}`,
-      begin: 'move',
-    });
-
-
-    me.appendChild(animation);
 
     this.el.addEventListener('raycaster-intersected', (e) => {
       console.log('Player hit something!', e);
-      this.el.emit('move');
+      const animation = document.createElement('a-animation');
+
+      const me = this.el;
+      const x = me.object3D.position.x;
+      const y = me.object3D.position.y;
+      const z = me.object3D.position.z;
+      const newX = x + 2;
+      const newY = y + 2;
+      const newZ = z + 2;
+  
+      setAttributes(animation, {
+        duration: '4000',
+        attribute: 'position',
+        from: `${x} ${y} ${z}`,
+        to: `${newX} ${newY} ${newZ}`,
+      });
+  
+  
+      me.appendChild(animation);
       // me.object3D.position.x += 1;
     });
   },
